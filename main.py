a710cf07f5c7363c6fd0a65079e80f0a655c94f0
Houssem Marzougui <marzouguih86@gmail.com>
2021-01-20 06:09:42 +0000

    Added the replit modulecto import clear()

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 import hangman_words
 import hangman_art
+from replit import clear
+
 
 chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
@@ -10,9 +12,6 @@ lives = 6
 
 print(hangman_art.logo)
 
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
-
 #Create blanks
 display = []
 for _ in range(word_length):
@@ -21,6 +20,9 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
     
+    #Use the clear() function imported from replit to clear the output between guesses.
+    clear()
+
     #If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
       print(f"You have already guessed: {guess}")
@@ -35,10 +37,10 @@ while not end_of_game:
     #Check if user is wrong.
     if guess not in chosen_word:
       print(f"You guessed {guess}, that is not in the word. You loose a life")
-        lives -= 1
-        if lives == 0:
-            end_of_game = True
-            print("You lose.")
+      lives -= 1
+      if lives == 0:
+          end_of_game = True
+          print("You lose.")
 
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
